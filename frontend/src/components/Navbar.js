6042cd8f87df2479a3604658d932150fb189b285
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2022-05-05 23:01:24 +0530

    updated brand name and style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -18,7 +18,9 @@ class NavBar extends Component {
                 backgroundColor: "#14202A",
             }}>
                 <div className="container-fluid">
-                    <div className="navbar-brand">CopyTxt</div>
+                    <div className="navbar-brand" style={{
+                        fontWeight: "bold"
+                    }}>CopyTxT</div>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><HamburgerIcon/></button>
                     <div class="collapse navbar-collapse " id="navbarNav">
                         <ul class="navbar-nav">
