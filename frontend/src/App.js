5d41dd7a47223439091342de19b46bb0f938e65f
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2022-06-10 01:31:22 +0530

    added syntax highlighting feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useRef } from 'react';
+import React, { Component } from 'react';
 import {
   Button,
   Tooltip,
@@ -10,15 +10,68 @@ import {
   useToast,
   Box,
   Text,
-  SkeletonText
+  SkeletonText,
+  Switch,
+  FormLabel,
+  Stack,
+  Select
 } from '@chakra-ui/react';
+// import Editor from 'react-simple-code-editor';
+// import { highlight, languages } from 'prismjs/components/prism-core';
+// import 'prismjs/components/prism-clike';
+// import 'prismjs/components/prism-javascript';
+// import 'prismjs/themes/prism.css'; 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { 
+  dark,
+  funky,
+  okaidia,
+  solarizedlight,
+  tomorrow,
+  twilight,
+  prism,
+  a11yDark,
+  atomDark,
+  base16AteliersulphurpoolLight,
+  cb,
+  coldarkCold,
+  coldarkDark,
+  coyWithoutShadows,
+  darcula,
+  dracula,
+  duotoneDark,
+  duotoneEarth,
+  duotoneForest,
+  duotoneLight,
+  duotoneSea,
+  duotoneSpace,
+  ghcolors,
+  gruvboxDark,
+  gruvboxLight,
+  holiTheme,
+  hopscotch,
+  lucario,
+  materialDark,
+  materialLight,
+  materialOceanic,
+  nightOwl,
+  nord,
+  oneDark,
+  oneLight,
+  pojoaque,
+  shadesOfPurple,
+  solarizedDarkAtom,
+  synthwave84,
+  vs,
+  vscDarkPlus,
+  xonokai,
+  zTouch } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import SidebarWithHeader from './components/Sidebar'
 import Navbar from './components/Navbar.js';
 import { Search2Icon, CopyIcon } from "@chakra-ui/icons";
 const axios = require("axios");
 import {Helmet} from "react-helmet";
 import favicon from "./favicon.png";
-
 const breakpoints = {
   base: "90%",
   sm: '90%',
@@ -26,6 +79,52 @@ const breakpoints = {
   lg: '70%',
   xl: '70%',
   '2xl': '70%',
+}
+
+const styles = {
+  "dark":dark,
+  "funky":funky,
+  "okaidia":okaidia,
+  "solarizedlight":solarizedlight,
+  "tomorrow":tomorrow,
+  "twilight":twilight,
+  "prism":prism,
+  "a11yDark":a11yDark,
+  "atomDark":atomDark,
+  "base16AteliersulphurpoolLight":base16AteliersulphurpoolLight,
+  "cb":cb,
+  "coldarkCold":coldarkCold,
+  "coldarkDark":coldarkDark,
+  "coyWithoutShadows":coyWithoutShadows,
+  "darcula":darcula,
+  "dracula":dracula,
+  "duotoneDark":duotoneDark,
+  "duotoneEarth":duotoneEarth,
+  "duotoneForest":duotoneForest,
+  "duotoneLight":duotoneLight,
+  "duotoneSea":duotoneSea,
+  "duotoneSpace":duotoneSpace,
+  "ghcolors":ghcolors,
+  "gruvboxDark":gruvboxDark,
+  "gruvboxLight":gruvboxLight,
+  "holiTheme":holiTheme,
+  "hopscotch":hopscotch,
+  "lucario":lucario,
+  "materialDark":materialDark,
+  "materialLight":materialLight,
+  "materialOceanic":materialOceanic,
+  "nightOwl":nightOwl,
+  "nord":nord,
+  "oneDark":oneDark,
+  "oneLight":oneLight,
+  "pojoaque":pojoaque,
+  "shadesOfPurple":shadesOfPurple,
+  "solarizedDarkAtom":solarizedDarkAtom,
+  "synthwave84":synthwave84,
+  "vs":vs,
+  "vscDarkPlus":vscDarkPlus,
+  "xonokai":xonokai,
+  "zTouch":zTouch
 }
 
 function CreatedToast(props){
@@ -117,7 +216,11 @@ class App extends Component {
       error:false,
       subFailed: true,
       loading: false,
-      started: null
+      started: null,
+      switch: false,
+      code: "",
+      language: "", 
+      theme: undefined
     }
   }
 
@@ -153,7 +256,13 @@ class App extends Component {
       console.log(`value: ${this.state.value} loading on`)
       this.setState({loading: true})
       axios.post("https://onclip.herokuapp.com/paste", {
-        data: this.state.value
+        data: this.state.value,
+        meta: {
+          isCode: this.state.switch,
+          language: this.state.language,
+          theme: this.state.theme
+        },
+
       }, 
       {
         "accept": "application/json",
@@ -212,7 +321,118 @@ class App extends Component {
           borderRadius: "10px",
           padding: "20px",
         }}>
+          <Stack display='flex' spacing={2} alignItems='center' justifyContent="flex-end" direction="row" mb="10px">
+            <><FormLabel htmlFor="syntax">Syntax Highlighting: </FormLabel>
+          <Switch id="syntax" isChecked={this.state.switch} onChange={()=> this.setState({switch: !this.state.switch})}/>  </>
+          
+          <Select variant='filled' isDisabled={!this.state.switch} placeholder='default' maxW={150} onChange={(e) => {
+            this.setState({
+              theme: e.target.value
+            })
+            console.log(this.state.theme)
+            console.log(e.target.value)
+          }}>
+            <option value="dark">dark</option>
+            <option value="funky">funky</option>
+            <option value="okaidia">okaidia</option>
+            <option value="solarizedlight">solarizedlight</option>
+            <option value="tomorrow">tomorrow</option>
+            <option value="twilight">twilight</option>
+            <option value="prism">prism</option>
+            <option value="a11yDark">a11yDark</option>
+            <option value="atomDark">atomDark</option>
+            <option value="base16AteliersulphurpoolLight">base16AteliersulphurpoolLight</option>
+            <option value="cb">cb</option>
+            <option value="coldarkCold">coldarkCold</option>
+            <option value="coldarkDark">coldarkDark</option>
+            <option value="coyWithoutShadows">coyWithoutShadows</option>
+            <option value="darcula">darcula</option>
+            <option value="duotoneDark">duotoneDark</option>
+            <option value="duotoneEarth">duotoneEarth</option>
+            <option value="duotoneForest">duotoneForest</option>
+            <option value="duotoneLight">duotoneLight</option>
+            <option value="duotoneSea">duotoneSea</option>
+            <option value="duotoneSpace">duotoneSpace</option>
+            <option value="ghcolors">ghcolors</option>
+            <option value="gruvboxDark">gruvboxDark</option>
+            <option value="gruvboxLight">gruvboxLight</option>
+            <option value="holiTheme">holiTheme</option>
+            <option value="hopscotch">hopscotch</option>
+            <option value="lucario">lucario</option>
+            <option value="materialDark">materialDark</option>
+            <option value="materialLight">materialLight</option>
+            <option value="materialOceanic">materialOceanic</option>
+            <option value="nightOwl">nightOwl</option>
+            <option value="nord">nord</option>
+            <option value="oneDark">oneDark</option>
+            <option value="oneLight">oneLight</option>
+            <option value="pojoaque">pojoaque</option>
+            <option value="shadesOfPurple">shadesOfPurple</option>
+            <option value="solarizedDarkAtom">solarizedDarkAtom</option>
+            <option value="synthwave84">synthwave84</option>
+            <option value="vs">vs</option>
+            <option value="vscDarkPlus">vscDarkPlus</option>
+            <option value="xonokai">xonokai</option>
+            <option value="zTouch">zTouch</option>   
+          </Select>
+          <Select variant='filled' isDisabled={!this.state.switch} placeholder='Language' onChange={(e) => {
+            this.setState({
+              language: e.target.value
+            })
+          }} maxW={150} >
+            <option value="1c">1c</option>
+            <option value="abnf">abnf</option>
+            <option value="accesslog">accesslog</option>
+            <option value="actionscript">actionscript</option>
+            <option value="ada">ada</option>
+            <option value="angelscript">angelscript</option>
+            <option value="apache">apache</option>
+            <option value="applescript">applescript</option>
+            <option value="arcade">arcade</option>
+            <option value="arduino">arduino</option>
+            <option value="armasm">armasm</option>
+            <option value="asciidoc">asciidoc</option>
+            <option value="aspectj">aspectj</option>
+            <option value="autohotkey">autohotkey</option>
+            <option value="autoit">autoit</option>
+            <option value="avrasm">avrasm</option>
+            <option value="awk">awk</option>
+            <option value="axapta">axapta</option>
+            <option value="bash">bash</option>
+            <option value="basic">basic</option>
+            <option value="bnf">bnf</option>
+            <option value="brainfuck">brainfuck</option>
+            <option value="c-like">c-like</option><option value="c">c</option>
+            <option value="cal">cal</option>
+            <option value="capnproto">capnproto</option>
+            <option value="ceylon">ceylon</option>
+            <option value="clean">clean</option>
+            <option value="clojure-repl">clojure-repl</option>
+            <option value="clojure">clojure</option>
+            <option value="cmake">cmake</option>
+            <option value="coffeescript">coffeescript</option>
+            <option value="coq">coq</option>
+            <option value="cos">cos</option>
+            <option value="cpp">cpp</option>
+            <option value="crmsh">crmsh</option>
+            <option value="crystal">crystal</option>
+            <option value="csharp">csharp</option>
+            <option value="csp">csp</option>
+            <option value="css">css</option>
+            <option value="d">d</option>
+            <option value="dart">dart</option><option value="delphi">delphi</option><option value="diff">diff</option><option value="django">django</option><option value="dns">dns</option><option value="dockerfile">dockerfile</option><option value="dos">dos</option><option value="dsconfig">dsconfig</option><option value="dts">dts</option><option value="dust">dust</option><option value="ebnf">ebnf</option><option value="elixir">elixir</option><option value="elm">elm</option><option value="erb">erb</option><option value="erlang-repl">erlang-repl</option><option value="erlang">erlang</option><option value="excel">excel</option><option value="fix">fix</option><option value="flix">flix</option><option value="fortran">fortran</option><option value="fsharp">fsharp</option><option value="gams">gams</option><option value="gauss">gauss</option><option value="gcode">gcode</option><option value="gherkin">gherkin</option><option value="glsl">glsl</option><option value="gml">gml</option><option value="go">go</option><option value="golo">golo</option><option value="gradle">gradle</option><option value="groovy">groovy</option><option value="haml">haml</option><option value="handlebars">handlebars</option><option value="haskell">haskell</option><option value="haxe">haxe</option><option value="hsp">hsp</option><option value="htmlbars">htmlbars</option><option value="http">http</option><option value="hy">hy</option><option value="inform7">inform7</option><option value="ini">ini</option><option value="irpf90">irpf90</option><option value="isbl">isbl</option><option value="java">java</option><option value="javascript">javascript</option><option value="jboss-cli">jboss-cli</option><option value="json">json</option><option value="jsx">jsx</option><option value="julia-repl">julia-repl</option><option value="julia">julia</option><option value="kotlin">kotlin</option><option value="lasso">lasso</option><option value="latex">latex</option><option value="ldif">ldif</option><option value="leaf">leaf</option><option value="less">less</option><option value="lisp">lisp</option><option value="livecodeserver">livecodeserver</option><option value="livescript">livescript</option><option value="llvm">llvm</option><option value="lsl">lsl</option><option value="lua">lua</option><option value="makefile">makefile</option><option value="markdown">markdown</option><option value="mathematica">mathematica</option><option value="matlab">matlab</option><option value="maxima">maxima</option><option value="mel">mel</option><option value="mercury">mercury</option><option value="mipsasm">mipsasm</option><option value="mizar">mizar</option><option value="mojolicious">mojolicious</option><option value="monkey">monkey</option><option value="moonscript">moonscript</option><option value="n1ql">n1ql</option><option value="nginx">nginx</option><option value="nim">nim</option><option value="nix">nix</option><option value="node-repl">node-repl</option><option value="nsis">nsis</option><option value="objectivec">objectivec</option><option value="ocaml">ocaml</option><option value="openscad">openscad</option><option value="oxygene">oxygene</option><option value="parser3">parser3</option><option value="perl">perl</option><option value="pf">pf</option><option value="pgsql">pgsql</option><option value="php-template">php-template</option><option value="php">php</option><option value="plaintext">plaintext</option><option value="pony">pony</option><option value="powershell">powershell</option><option value="processing">processing</option><option value="profile">profile</option><option value="prolog">prolog</option><option value="properties">properties</option><option value="protobuf">protobuf</option><option value="puppet">puppet</option><option value="purebasic">purebasic</option><option value="python-repl">python-repl</option><option value="python">python</option><option value="q">q</option><option value="qml">qml</option><option value="r">r</option><option value="reasonml">reasonml</option><option value="rib">rib</option><option value="roboconf">roboconf</option><option value="routeros">routeros</option><option value="rsl">rsl</option><option value="ruby">ruby</option><option value="ruleslanguage">ruleslanguage</option><option value="rust">rust</option><option value="sas">sas</option><option value="scala">scala</option><option value="scheme">scheme</option><option value="scilab">scilab</option><option value="scss">scss</option><option value="shell">shell</option><option value="smali">smali</option><option value="smalltalk">smalltalk</option><option value="sml">sml</option><option value="sqf">sqf</option><option value="sql">sql</option><option value="sql_more">sql_more</option><option value="stan">stan</option><option value="stata">stata</option><option value="step21">step21</option><option value="stylus">stylus</option><option value="subunit">subunit</option><option value="swift">swift</option><option value="taggerscript">taggerscript</option><option value="tap">tap</option><option value="tcl">tcl</option><option value="thrift">thrift</option><option value="tp">tp</option><option value="twig">twig</option><option value="typescript">typescript</option><option value="vala">vala</option><option value="vbnet">vbnet</option><option value="vbscript-html">vbscript-html</option><option value="vbscript">vbscript</option><option value="verilog">verilog</option><option value="vhdl">vhdl</option><option value="vim">vim</option><option value="x86asm">x86asm</option><option value="xl">xl</option><option value="xml">xml</option><option value="xquery">xquery</option><option value="yaml">yaml</option><option value="zephir">zephir</option>
+          </Select>
+          </Stack>
+          {this.state.switch ? <SyntaxHighlighter language={this.state.language} style={styles[this.state.theme]}>
+            {this.state.value}
+          </SyntaxHighlighter>: <></>}
+          
+          {/* <Editor
+          onValueChange={code => this.setState({code: code})}
+          highlight={code => highlight(code, languages.js)}
+          /> */}
           <Textarea
+              mt={5}
              placeholder="Paste your text here"
              colorScheme="white"
              color="black"
@@ -229,10 +449,12 @@ class App extends Component {
                   })
                 }else if(e.target.value !=="" && this.state.error === true){
                  this.setState({error:false,
+                  value: e.target.value,
                 subFailed: false})
                }else{
                  this.setState({
                    error:true,
+                   value: e.target.value,
                    subFailed: true
                  })
                }
